92c5325e00f1426ab693407f8b7a5680f2d71462
darken <darken@darken.eu>
2019-07-08 00:06:27 +0200

    Removed unused code

--- app/src/main/java/eu/darken/androidkotlinstarter/ActivityBinder.kt
+++ app/src/main/java/eu/darken/androidkotlinstarter/ActivityBinder.kt
@@ -2,13 +2,12 @@ package eu.darken.androidkotlinstarter
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import eu.darken.androidkotlinstarter.main.ui.IntentModule
 import eu.darken.androidkotlinstarter.main.ui.MainActivity
 import eu.darken.androidkotlinstarter.main.ui.MainActivityModule
 
 
 @Module
 abstract class ActivityBinder {
-    @ContributesAndroidInjector(modules = [MainActivityModule::class, IntentModule::class])
+    @ContributesAndroidInjector(modules = [MainActivityModule::class])
     abstract fun mainActivity(): MainActivity
 }
